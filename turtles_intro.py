91903648b80306df3e5272dde3386e47b98fca68
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2022-07-02 22:47:28 +0300

    Add files via upload

---
+++ turtles_intro.py
@@ -0,0 +1,33 @@
+'''import turtle
+turtle.shape("turtle")
+for i in range(0,5):
+    turtle.forward(100)
+    turtle.right(72)
+turtle.exitonclick()
+'''
+
+import turtle
+for i in range(0,10):
+    turtle.right(100)
+    for i in range(0,5):
+        turtle.forward(100)
+        turtle.right(72)
+turtle.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
